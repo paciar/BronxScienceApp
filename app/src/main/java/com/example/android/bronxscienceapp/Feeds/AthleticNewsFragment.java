41ba4c37a524c0aeb5677f5c3f4f46b1125552e5
paciar <paciar@bxscience.edu>
2022-05-26 10:13:29 -0400

    V1.0.4 - all RSS feeds implemented

---
+++ app/src/main/java/com/example/android/bronxscienceapp/Feeds/AthleticNewsFragment.java
@@ -0,0 +1,137 @@
+package com.example.android.bronxscienceapp.Feeds;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.bronxscienceapp.CALENDAR_Adapter.FeedAdapter;
+import com.example.android.bronxscienceapp.CALENDAR_Common.HTTPDataHandler;
+import com.example.android.bronxscienceapp.CALENDAR_Model.RSSObject;
+import com.example.android.bronxscienceapp.R;
+import com.google.gson.Gson;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AthleticNewsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AthleticNewsFragment extends Fragment {
+
+    RecyclerView mRecyclerView;
+    RSSObject mRSSObject;
+
+    //RSS Link
+    private final String RSS_link = "https://bxscience.edu/apps/news/news_rss.jsp?id=30";
+    private final String RSS_to_Json_API = "https://api.rss2json.com/v1/api.json?rss_url=";
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public AthleticNewsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment AthleticNewsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AthleticNewsFragment newInstance(String param1, String param2) {
+        AthleticNewsFragment fragment = new AthleticNewsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_news, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity().getBaseContext(), LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+
+        loadRSS();
+
+        return view;
+    }
+    private void loadRSS() {
+        AsyncTask<String, String, String> loadRSSAsync = new AsyncTask<String, String, String>() {
+
+            ProgressDialog mDialog = new ProgressDialog(getActivity());
+
+            @Override
+            protected void onPreExecute() {
+                mDialog.setMessage("Please wait...");
+                mDialog.show();
+            }
+
+            @Override
+            protected String doInBackground(String... strings) {
+                String result;
+                HTTPDataHandler http = new HTTPDataHandler();
+                result = http.GetHTTPData(strings[0]);
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                mDialog.dismiss();
+                mRSSObject = new Gson().fromJson(s, RSSObject.class);
+                FeedAdapter adapter = new FeedAdapter(mRSSObject, getActivity().getBaseContext());
+                mRecyclerView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+            }
+        };
+
+        StringBuilder url_get_data = new StringBuilder(RSS_to_Json_API);
+        url_get_data.append(RSS_link);
+        loadRSSAsync.execute(url_get_data.toString());
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        getActivity().getMenuInflater().inflate(R.menu.main_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_refresh)
+            loadRSS();
+        return true;
+    }
+}
